431bf87300418af0b586649409093fbe3f50d6a6
SeolHuiGwan9478 <shg5977@naver.com>
2024-06-11 11:40:51 +0900

    First commit

---
+++ src/test/java/woozlabs/echo/EchoApplicationTests.java
@@ -0,0 +1,13 @@
+package woozlabs.echo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
