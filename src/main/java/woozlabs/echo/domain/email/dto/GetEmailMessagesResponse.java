b99c8bff258112c216eedee6db15f0e8371f5ca5
SeolHuiGwan9478 <shg5977@naver.com>
2024-06-12 12:33:51 +0900

    Remove: 기존에 생성했던 Email 관련 파일 제거

--- src/main/java/woozlabs/echo/domain/email/dto/GetEmailMessagesResponse.java
+++
@@ -1,8 +0,0 @@
-package woozlabs.echo.domain.email.dto;
-
-import lombok.Getter;
-import woozlabs.echo.global.dto.ResponseDto;
-
-@Getter
-public class GetEmailMessagesResponse implements ResponseDto {
-}
--- src/main/java/woozlabs/echo/domain/email/service/EmailService.java
+++
@@ -1,7 +0,0 @@
-package woozlabs.echo.domain.email.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class EmailService {
-}
