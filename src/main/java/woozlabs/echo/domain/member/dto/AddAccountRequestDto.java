5a66f96ad34181963a7d582deb0a9e359059a7ae
DongHyeok Lim <jh07050@gmail.com>
2024-06-24 16:04:19 +0900

    Feat: AddAccountRequest Dto 설계

---
+++ src/main/java/woozlabs/echo/domain/member/dto/AddAccountRequestDto.java
@@ -0,0 +1,16 @@
+package woozlabs.echo.domain.member.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddAccountRequestDto {
+
+    private String uid;
+    private String displayName;
+    private String email;
+    private boolean emailVerified;
+    private String photoURL;
+    private String googleAccessToken;
+}
