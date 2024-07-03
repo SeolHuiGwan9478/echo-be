90731c6817545f149a8ce2851248d156e2eb6cb5
DongHyeok Lim <jh07050@gmail.com>
2024-07-03 16:03:51 +0900

    Refactor: Redis 제거로 인한 Token 관리 로직 제거

--- src/main/java/woozlabs/echo/global/token/repository/AccessTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package woozlabs.echo.global.token.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import woozlabs.echo.global.token.entity.AccessToken;
-
-public interface AccessTokenRepository extends CrudRepository<AccessToken, Long> {
-}
