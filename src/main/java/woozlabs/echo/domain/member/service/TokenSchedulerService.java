0f9db7e96a886bc2f45d96338d2cf5b56460b425
DongHyeok Lim <jh07050@gmail.com>
2024-07-12 21:29:26 +0900

    Refactor: 스케줄링 주기 5분으로 변경

--- src/main/java/woozlabs/echo/domain/member/service/TokenSchedulerService.java
+++ src/main/java/woozlabs/echo/domain/member/service/TokenSchedulerService.java
@@ -29,7 +29,7 @@ public class TokenSchedulerService {
     private final SubAccountRepository subAccountRepository;
     private final GoogleOAuthUtils googleOAuthUtils;
 
-    @Scheduled(fixedRate = 60000)
+    @Scheduled(fixedRate = 5 * 60 * 1000)
     @Transactional
     public void checkAndRefreshTokens() {
         LocalDateTime cutoffTime = LocalDateTime.now().minus(57, ChronoUnit.MINUTES);
