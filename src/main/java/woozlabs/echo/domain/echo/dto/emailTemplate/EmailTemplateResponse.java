1c80f4bdd2eb235eb4ce07540ec2ad4599c9ba1c
DongHyeok Lim <jh07050@gmail.com>
2024-07-22 16:39:17 +0900

    Feat: to - Recipients에서 Email을 받아와서 List로 처리

--- src/main/java/woozlabs/echo/domain/echo/dto/emailTemplate/EmailTemplateResponse.java
+++ src/main/java/woozlabs/echo/domain/echo/dto/emailTemplate/EmailTemplateResponse.java
@@ -1,10 +1,12 @@
 package woozlabs.echo.domain.echo.dto.emailTemplate;
 
 import lombok.*;
+import woozlabs.echo.domain.echo.entity.EmailRecipient;
 import woozlabs.echo.domain.echo.entity.EmailTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -23,7 +25,9 @@ public class EmailTemplateResponse {
         this.key = emailTemplate.getId();
         this.templateName = emailTemplate.getTemplateName();
         this.subject = emailTemplate.getSubject();
-        this.to = emailTemplate.getTo();
+        this.to = emailTemplate.getRecipients().stream()
+                .map(EmailRecipient::getEmail)
+                .collect(Collectors.toList());
         this.body = emailTemplate.getBody();
     }
 }
