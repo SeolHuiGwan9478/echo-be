038d59339ffd62eeb78935d0d6f174de4a0fa376
SeolHuiGwan9478 <shg5977@naver.com>
2024-07-24 16:30:42 +0900

    Fix: ErrorMessage 디테일 메세지 수정

--- src/main/java/woozlabs/echo/domain/gmail/controller/GmailController.java
+++ src/main/java/woozlabs/echo/domain/gmail/controller/GmailController.java
@@ -47,7 +47,8 @@ public class GmailController {
         }catch (IOException e){
             throw new CustomErrorException(ErrorCode.REQUEST_GMAIL_USER_THREADS_GET_API_ERROR_MESSAGE, e.getMessage());
         }catch (Exception e){
-            throw new CustomErrorException(ErrorCode.FAILED_TO_GET_GMAIL_CONNECTION_REQUEST, e.getMessage());
+            throw new RuntimeException(e.getMessage());
+            //throw new CustomErrorException(ErrorCode.FAILED_TO_GET_GMAIL_CONNECTION_REQUEST, e.getMessage());
         }
     }
 
@@ -80,7 +81,8 @@ public class GmailController {
         }catch (IOException e){
             throw new CustomErrorException(ErrorCode.REQUEST_GMAIL_USER_THREADS_GET_API_ERROR_MESSAGE, e.getMessage());
         }catch (Exception e){
-            throw new CustomErrorException(ErrorCode.FAILED_TO_GET_GMAIL_CONNECTION_REQUEST, e.getMessage());
+            throw new RuntimeException(e.getMessage());
+            //throw new CustomErrorException(ErrorCode.FAILED_TO_GET_GMAIL_CONNECTION_REQUEST, e.getMessage());
         }
     }
 
