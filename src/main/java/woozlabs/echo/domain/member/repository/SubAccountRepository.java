ea2f3c17dde0a6a1a8283c16afdf95989b00c741
DongHyeok Lim <jh07050@gmail.com>
2024-07-26 16:44:25 +0900

    Refactor: SubAccount 제거

--- src/main/java/woozlabs/echo/domain/member/repository/SubAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package woozlabs.echo.domain.member.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import woozlabs.echo.domain.member.entity.SubAccount;
-
-public interface SubAccountRepository extends JpaRepository<SubAccount, Long> {
-}
