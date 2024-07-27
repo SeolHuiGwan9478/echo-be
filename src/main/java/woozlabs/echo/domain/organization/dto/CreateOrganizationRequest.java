fd7e3ff8658d512295bcb9259e55487c7edd6e68
DongHyeok Lim <jh07050@gmail.com>
2024-07-27 19:51:03 +0900

    Feat: Create Organization Dto

---
+++ src/main/java/woozlabs/echo/domain/organization/dto/CreateOrganizationRequest.java
@@ -0,0 +1,11 @@
+package woozlabs.echo.domain.organization.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateOrganizationRequest {
+
+    private String name;
+}
