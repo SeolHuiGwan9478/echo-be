f7971ca93be76722cb15a1c38dcc2645a6aeafba
DongHyeok Lim <jh07050@gmail.com>
2024-07-28 16:43:14 +0900

    Feat: 조직 내 설정한 메일주소만 불러오기

--- src/main/java/woozlabs/echo/domain/organization/dto/OrganizationResponse.java
+++ src/main/java/woozlabs/echo/domain/organization/dto/OrganizationResponse.java
@@ -4,8 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import woozlabs.echo.domain.member.entity.Member;
 import woozlabs.echo.domain.organization.entity.Organization;
+
+import java.util.List;
 
 @Getter
 @Builder
@@ -15,9 +16,11 @@ public class OrganizationResponse {
 
     private String organizationName;
     private OrganizationToMemberDto owner;
+    private List<String> memberEmails;
 
     public OrganizationResponse(Organization organization) {
         this.organizationName = organization.getName();
         this.owner = new OrganizationToMemberDto(organization.getOwner());
+        this.memberEmails = organization.getEmails();
     }
 }
