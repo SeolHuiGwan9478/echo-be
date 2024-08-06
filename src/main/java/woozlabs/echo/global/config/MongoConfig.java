4803054a6b08aa01d906a3835c443e4890a12302
DongHyeok Lim <jh07050@gmail.com>
2024-08-06 16:13:22 +0900

    Chore: MongoDB Setting

---
+++ src/main/java/woozlabs/echo/global/config/MongoConfig.java
@@ -0,0 +1,13 @@
+package woozlabs.echo.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+@Configuration
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Override
+    protected String getDatabaseName() {
+        return "echo_mongo";
+    }
+}
