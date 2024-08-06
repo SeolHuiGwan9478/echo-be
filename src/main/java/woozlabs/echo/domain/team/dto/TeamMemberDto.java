f430cb23a47a150866e510eb8d6837fa40893011
DongHyeok Lim <jh07050@gmail.com>
2024-08-06 17:26:59 +0900

    Feat: Team 생성, 읽기 Dto

---
+++ src/main/java/woozlabs/echo/domain/team/dto/TeamMemberDto.java
@@ -0,0 +1,13 @@
+package woozlabs.echo.domain.team.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import woozlabs.echo.domain.team.entity.TeamMember;
+
+@Getter
+@Builder
+public class TeamMemberDto {
+    private Long id;
+    private String memberUid;
+    private TeamMember.TeamMemberRole role;
+}
