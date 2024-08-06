270d980c7d76a94d1bc4b6c595c8be7852b2c890
DongHyeok Lim <jh07050@gmail.com>
2024-08-06 16:37:31 +0900

    Feat: SharedEmailInbox, Private Comment 초안 설계

---
+++ src/main/java/woozlabs/echo/domain/team/entity/SharedEmail.java
@@ -0,0 +1,23 @@
+package woozlabs.echo.domain.team.entity;
+
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Document(collection = "shared_emails")
+public class SharedEmail {
+
+    @Id
+    private String id;
+    private String teamId;  // MySQL의 Team 엔티티 ID 참조
+    private String threadId;
+    private String subject;
+    private String sharedById;  // MySQL의 Member 엔티티 ID 참조
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
