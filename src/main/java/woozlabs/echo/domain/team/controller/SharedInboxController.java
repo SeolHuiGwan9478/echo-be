6bc0297ff245576a292fa677bb686b4184dc379f
DongHyeok Lim <jh07050@gmail.com>
2024-08-14 19:04:51 +0900

    Feat: Thread 공유 및 불러오기 API 구현

--- src/main/java/woozlabs/echo/domain/team/controller/SharedInboxController.java
+++ src/main/java/woozlabs/echo/domain/team/controller/SharedInboxController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import woozlabs.echo.domain.team.dto.ShareEmailRequestDto;
 import woozlabs.echo.domain.team.entity.SharedEmail;
+import woozlabs.echo.domain.team.entity.Thread;
 import woozlabs.echo.domain.team.service.SharedInboxService;
 import woozlabs.echo.global.constant.GlobalConstant;
 
@@ -18,15 +19,24 @@ public class SharedInboxController {
 
     private final SharedInboxService sharedInboxService;
 
-    @GetMapping("/team/{teamId}")
+    @GetMapping("/emails/team/{teamId}")
     public ResponseEntity<List<SharedEmail>> getSharedEmailsByTeam(@PathVariable("teamId") String teamId) {
         List<SharedEmail> allEmails = sharedInboxService.getSharedEmailsByTeam(teamId);
         return ResponseEntity.ok(allEmails);
     }
 
-    @PostMapping("/share")
+    @GetMapping("/thread/{threadId}")
+    public ResponseEntity<Thread> getSharedThread(@PathVariable("threadId") String threadId) {
+        Thread thread = sharedInboxService.getSharedThread(threadId);
+        if (thread == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(thread);
+    }
+
+    @PostMapping("/thread/share")
     public ResponseEntity<Void> shareEmail(HttpServletRequest httpServletRequest,
-                                                  @RequestBody ShareEmailRequestDto shareEmailRequestDto) {
+                                           @RequestBody ShareEmailRequestDto shareEmailRequestDto) {
         String sharedById = (String) httpServletRequest.getAttribute(GlobalConstant.FIREBASE_UID_KEY);
         sharedInboxService.shareEmail(sharedById, shareEmailRequestDto);
         return ResponseEntity.status(201).build();
--- src/main/java/woozlabs/echo/domain/team/service/SharedInboxService.java
+++ src/main/java/woozlabs/echo/domain/team/service/SharedInboxService.java
@@ -2,9 +2,12 @@ package woozlabs.echo.domain.team.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import woozlabs.echo.domain.gmail.dto.thread.GmailThreadGetResponse;
 import woozlabs.echo.domain.team.dto.ShareEmailRequestDto;
 import woozlabs.echo.domain.team.entity.SharedEmail;
+import woozlabs.echo.domain.team.entity.Thread;
 import woozlabs.echo.domain.team.repository.SharedInboxRepository;
+import woozlabs.echo.domain.team.repository.ThreadRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,16 +17,31 @@ import java.util.List;
 public class SharedInboxService {
 
     private final SharedInboxRepository sharedInboxRepository;
+    private final ThreadRepository threadRepository;
 
     public List<SharedEmail> getSharedEmailsByTeam(String teamId) {
         return sharedInboxRepository.findByTeamId(teamId);
     }
 
+    public Thread getSharedThread(String threadId) {
+        return threadRepository.findById(threadId).orElse(null);
+    }
+
     public void shareEmail(String sharedByUid, ShareEmailRequestDto shareEmailRequestDto) {
+
+        Thread thread = Thread.builder()
+                .id(shareEmailRequestDto.getGmailThread().getId())
+                .historyId(shareEmailRequestDto.getGmailThread().getHistoryId())
+                .messages(shareEmailRequestDto.getGmailThread().getMessages())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        Thread saveThread = threadRepository.save(thread);
+
         SharedEmail sharedEmail = SharedEmail.builder()
                 .teamId(shareEmailRequestDto.getTeamId())
-                .threadId(shareEmailRequestDto.getThreadId())
-                .subject(shareEmailRequestDto.getSubject())
+                .threadId(saveThread.getId())
                 .sharedById(sharedByUid)
                 .createdAt(LocalDateTime.now())
                 .updatedAt(LocalDateTime.now())
