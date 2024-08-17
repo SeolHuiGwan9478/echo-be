a42b025637c6ed4325a7bab4471fe0a385906c5e
DongHyeok Lim <jh07050@gmail.com>
2024-08-17 17:26:26 +0900

    Feat: 멤버의 권한에 따른 접근 제어 기능 추가

--- src/main/java/woozlabs/echo/domain/team/controller/SharedInboxController.java
+++ src/main/java/woozlabs/echo/domain/team/controller/SharedInboxController.java
@@ -20,14 +20,18 @@ public class SharedInboxController {
     private final SharedInboxService sharedInboxService;
 
     @GetMapping("/emails/team/{teamId}")
-    public ResponseEntity<List<SharedEmail>> getSharedEmailsByTeam(@PathVariable("teamId") String teamId) {
-        List<SharedEmail> allEmails = sharedInboxService.getSharedEmailsByTeam(teamId);
+    public ResponseEntity<List<SharedEmail>> getSharedEmailsByTeam(HttpServletRequest httpServletRequest,
+                                                                   @PathVariable("teamId") String teamId) {
+        String uid = (String) httpServletRequest.getAttribute(GlobalConstant.FIREBASE_UID_KEY);
+        List<SharedEmail> allEmails = sharedInboxService.getSharedEmailsByTeam(uid, teamId);
         return ResponseEntity.ok(allEmails);
     }
 
     @GetMapping("/thread/{threadId}")
-    public ResponseEntity<Thread> getSharedThread(@PathVariable("threadId") String threadId) {
-        Thread thread = sharedInboxService.getSharedThread(threadId);
+    public ResponseEntity<Thread> getSharedThread(HttpServletRequest httpServletRequest,
+                                                  @PathVariable("threadId") String threadId) {
+        String uid = (String) httpServletRequest.getAttribute(GlobalConstant.FIREBASE_UID_KEY);
+        Thread thread = sharedInboxService.getSharedThread(uid, threadId);
         if (thread == null) {
             return ResponseEntity.notFound().build();
         }
--- src/main/java/woozlabs/echo/domain/team/service/SharedInboxService.java
+++ src/main/java/woozlabs/echo/domain/team/service/SharedInboxService.java
@@ -2,32 +2,54 @@ package woozlabs.echo.domain.team.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import woozlabs.echo.domain.gmail.dto.thread.GmailThreadGetResponse;
 import woozlabs.echo.domain.team.dto.ShareEmailRequestDto;
 import woozlabs.echo.domain.team.entity.SharedEmail;
+import woozlabs.echo.domain.team.entity.TeamMember;
 import woozlabs.echo.domain.team.entity.Thread;
 import woozlabs.echo.domain.team.repository.SharedInboxRepository;
 import woozlabs.echo.domain.team.repository.ThreadRepository;
+import woozlabs.echo.domain.team.utils.AuthorizationUtil;
+import woozlabs.echo.global.exception.CustomErrorException;
+import woozlabs.echo.global.exception.ErrorCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class SharedInboxService {
 
     private final SharedInboxRepository sharedInboxRepository;
     private final ThreadRepository threadRepository;
+    private final TeamService teamService;
 
-    public List<SharedEmail> getSharedEmailsByTeam(String teamId) {
+    public List<SharedEmail> getSharedEmailsByTeam(String uid, String teamId) {
+        teamService.getTeamMember(uid, Long.parseLong(teamId));
         return sharedInboxRepository.findByTeamId(teamId);
     }
 
-    public Thread getSharedThread(String threadId) {
+    public Thread getSharedThread(String uid, String threadId) {
+        SharedEmail sharedEmail = sharedInboxRepository.findByThreadId(threadId)
+                .orElseThrow(() -> new CustomErrorException(ErrorCode.NOT_FOUND_SHARED_EMAIL));
+
+        TeamMember teamMember =  teamService.getTeamMember(uid, Long.parseLong(sharedEmail.getTeamId()));
+        if (!AuthorizationUtil.canViewSharedEmail(teamMember, sharedEmail.getShareStatus())) {
+            throw new CustomErrorException(ErrorCode.UNAUTHORIZED_ACCESS);
+        }
+
         return threadRepository.findById(threadId).orElse(null);
     }
 
+    @Transactional
     public void shareEmail(String sharedByUid, ShareEmailRequestDto shareEmailRequestDto) {
+        TeamMember teamMember = teamService.getTeamMember(sharedByUid, Long.parseLong(shareEmailRequestDto.getTeamId()));
+        if (!AuthorizationUtil.canEditSharedEmail(teamMember)) {
+            throw new CustomErrorException(ErrorCode.UNAUTHORIZED_ACCESS);
+        }
 
         Thread thread = Thread.builder()
                 .id(shareEmailRequestDto.getGmailThread().getId())
