641ad6170b75fa58d57a26af91772e31d1a6a8d2
DongHyeok Lim <jh07050@gmail.com>
2024-08-17 16:33:25 +0900

    Feat: 키와 내용 Map으로 수정

--- src/main/java/woozlabs/echo/domain/team/entity/PrivateComment.java
+++ src/main/java/woozlabs/echo/domain/team/entity/PrivateComment.java
@@ -2,19 +2,21 @@ package woozlabs.echo.domain.team.entity;
 
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @Getter
+@Setter
 @Document(collection = "private_comments")
 public class PrivateComment {
 
     @Id
     private String id;
-
-    private String sharedEmailId;  // MongoDB의 SharedEmail 문서 ID 참조
-    private String memberId;  // MySQL의 Member 엔티티 ID 참조
-    private String content;
+    private String sharedEmailId;
+    private String authorId;
+    private Map<String, String> encryptedContents;
     private LocalDateTime createdAt;
 }
