15f26aae7e66aada457ac9a06771938afd7c2ee7
DongHyeok Lim <jh07050@gmail.com>
2024-08-24 16:17:07 +0900

    Feat: Lombok Setter 설정

--- src/main/java/woozlabs/echo/domain/team/dto/TeamInvitationRequestDto.java
+++ src/main/java/woozlabs/echo/domain/team/dto/TeamInvitationRequestDto.java
@@ -1,9 +1,11 @@
 package woozlabs.echo.domain.team.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 import woozlabs.echo.domain.team.entity.TeamMemberRole;
 
 @Getter
+@Setter
 public class TeamInvitationRequestDto {
 
     private String inviteeEmail;
