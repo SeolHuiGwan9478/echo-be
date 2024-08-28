bc5da9e5f77ffc8098b4eace562af957c86a8ab4
DongHyeok Lim <jh07050@gmail.com>
2024-08-28 21:59:32 +0900

    Feat: Account Controller, Service

--- src/main/java/woozlabs/echo/domain/member/controller/AccountController.java
+++ src/main/java/woozlabs/echo/domain/member/controller/AccountController.java
@@ -1,15 +1,10 @@
 package woozlabs.echo.domain.member.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import woozlabs.echo.domain.member.dto.AccountDto;
 import woozlabs.echo.domain.member.dto.ProfileResponseDto;
-import woozlabs.echo.domain.member.dto.UpdatePreferenceRequestDto;
 import woozlabs.echo.domain.member.service.AccountService;
-import woozlabs.echo.domain.member.service.MemberService;
-import woozlabs.echo.global.constant.GlobalConstant;
 
 @RestController
 @RequestMapping("/api/v1/echo/user")
@@ -28,11 +23,5 @@ public class AccountController {
     public ResponseEntity<ProfileResponseDto> getProfileByUid(@PathVariable("uid") String uid) {
         ProfileResponseDto response = accountService.getProfileByField("uid", uid);
         return ResponseEntity.ok(response);
-    }
-
-    @PostMapping("/{uid}/preference")
-    public ResponseEntity<Void> updatePreference(@PathVariable("uid") String uid, @RequestBody UpdatePreferenceRequestDto updatePreferenceRequestDto) {
-        accountService.updatePreference(uid, updatePreferenceRequestDto);
-        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/woozlabs/echo/domain/member/service/AccountService.java
+++ src/main/java/woozlabs/echo/domain/member/service/AccountService.java
@@ -5,15 +5,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import woozlabs.echo.domain.member.dto.*;
 import woozlabs.echo.domain.member.entity.Account;
-import woozlabs.echo.domain.member.entity.Member;
-import woozlabs.echo.domain.member.entity.Theme;
 import woozlabs.echo.domain.member.repository.AccountRepository;
-import woozlabs.echo.domain.member.repository.MemberRepository;
 import woozlabs.echo.global.exception.CustomErrorException;
 import woozlabs.echo.global.exception.ErrorCode;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -38,53 +32,12 @@ public class AccountService {
     private Account fetchMemberByField(String fieldType, String fieldValue) {
         if (fieldType.equals("email")) {
             return accountRepository.findByEmail(fieldValue)
-                    .orElseThrow(() -> new CustomErrorException(ErrorCode.NOT_FOUND_MEMBER_ERROR_MESSAGE));
+                    .orElseThrow(() -> new CustomErrorException(ErrorCode.NOT_FOUND_ACCOUNT_ERROR_MESSAGE));
         } else if (fieldType.equals("uid")) {
             return accountRepository.findByUid(fieldValue)
-                    .orElseThrow(() -> new CustomErrorException(ErrorCode.NOT_FOUND_MEMBER_ERROR_MESSAGE));
+                    .orElseThrow(() -> new CustomErrorException(ErrorCode.NOT_FOUND_ACCOUNT_ERROR_MESSAGE));
         } else {
             throw new CustomErrorException(ErrorCode.INVALID_FIELD_TYPE_ERROR_MESSAGE);
         }
     }
-
-    @Transactional
-    public void updatePreference(String uid, UpdatePreferenceRequestDto updatePreferenceRequest) {
-        Account account = accountRepository.findByUid(uid)
-                .orElseThrow(() -> new CustomErrorException(ErrorCode.NOT_FOUND_MEMBER_ERROR_MESSAGE));
-
-        Member member = account.getMember();
-        if (member == null) {
-            throw new CustomErrorException(ErrorCode.NOT_FOUND_MEMBER_ERROR_MESSAGE);
-        }
-
-        PreferenceDto preferenceDto = updatePreferenceRequest.getPreference();
-        AppearanceDto appearanceDto = preferenceDto.getAppearance();
-        NotificationDto notificationDto = preferenceDto.getNotification();
-
-        member.setLanguage(preferenceDto.getLang() != null ? preferenceDto.getLang() : "en");
-        member.setTheme(appearanceDto.getTheme() != null ? appearanceDto.getTheme() : Theme.SYSTEM);
-        member.setWatchNotification(notificationDto.getWatchNotification());
-        member.setMarketingEmails(notificationDto.isMarketingEmails());
-        member.setSecurityEmails(notificationDto.isSecurityEmails());
-
-        accountRepository.save(account);
-    }
-
-
-    // memberSErvice로 옮길것
-//    public PreferenceDto getPreference(String uid) {
-//        Member member = memberRepository.findByUid(uid);
-//
-//        return PreferenceDto.builder()
-//                .lang(member.getLanguage())
-//                .appearance(AppearanceDto.builder()
-//                        .theme(member.getTheme())
-//                        .build())
-//                .notification(NotificationDto.builder()
-//                        .watchNotification(member.getWatchNotification())
-//                        .marketingEmails(member.isMarketingEmails())
-//                        .securityEmails(member.isSecurityEmails())
-//                        .build())
-//                .build();
-//    }
 }
