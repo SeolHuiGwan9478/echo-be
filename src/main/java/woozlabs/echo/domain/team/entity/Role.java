134399a45df6bd6cef25531d779b75a4af1fa35b
DongHyeok Lim <jh07050@gmail.com>
2024-08-31 22:32:49 +0900

    Refactor: 역직렬화를 위한 Member Role 수정

--- src/main/java/woozlabs/echo/domain/team/entity/Role.java
+++ src/main/java/woozlabs/echo/domain/team/entity/Role.java
@@ -1,8 +1,18 @@
 package woozlabs.echo.domain.team.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum Role {
+
+    @JsonProperty("Admin")
     ADMIN,
+
+    @JsonProperty("Editor")
     EDITOR,
+
+    @JsonProperty("Viewer")
     VIEWER,
+
+    @JsonProperty("PublicViewer")
     PUBLIC_VIEWER
 }
