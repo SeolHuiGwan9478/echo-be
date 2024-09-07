1255b86d24a9dc6514da72c85d27b10565e17f67
DongHyeok Lim <jh07050@gmail.com>
2024-09-07 19:48:28 +0900

    Feat: SharedEmail Get 메서드 및 Post 기능 추가
    공유 이메일 시 본인을 Owner로 추가합니다.

--- src/main/java/woozlabs/echo/domain/sharedEmail/repository/SharedEmailPermissionRepository.java
+++ src/main/java/woozlabs/echo/domain/sharedEmail/repository/SharedEmailPermissionRepository.java
@@ -1,7 +1,13 @@
 package woozlabs.echo.domain.sharedEmail.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import woozlabs.echo.domain.sharedEmail.entity.Permission;
 import woozlabs.echo.domain.sharedEmail.entity.SharedEmailPermission;
 
+import java.util.Map;
+import java.util.Optional;
+
 public interface SharedEmailPermissionRepository extends JpaRepository<SharedEmailPermission, Long> {
+
+    Optional<SharedEmailPermission> findBySharedEmailId(Long sharedEmail_id);
 }
--- src/main/java/woozlabs/echo/domain/sharedEmail/repository/SharedInboxRepository.java
+++ src/main/java/woozlabs/echo/domain/sharedEmail/repository/SharedInboxRepository.java
@@ -3,5 +3,11 @@ package woozlabs.echo.domain.sharedEmail.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import woozlabs.echo.domain.sharedEmail.entity.SharedEmail;
 
+import java.util.Optional;
+
 public interface SharedInboxRepository extends JpaRepository<SharedEmail, Long> {
+
+    Optional<SharedEmail> findByDataId(String dataId);
+
+    boolean existsByDataId(String dataId);
 }
