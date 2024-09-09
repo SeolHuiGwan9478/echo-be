e7ba4385fa3e64f0a68ed570629b881404ae4ea4
DongHyeok Lim <jh07050@gmail.com>
2024-09-09 18:48:49 +0900

    Feat: 특정 메서드에 대해 Token 부분 검증
    특정 메서드에 대해 토큰이 있으면 추출하고 없을 경우 패스합니다.

--- src/main/java/woozlabs/echo/global/interceptor/FirebaseAuthInterceptor.java
+++ src/main/java/woozlabs/echo/global/interceptor/FirebaseAuthInterceptor.java
@@ -7,10 +7,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import woozlabs.echo.global.constant.GlobalConstant;
 import woozlabs.echo.global.utils.FirebaseTokenVerifier;
 
-import static woozlabs.echo.global.constant.GlobalConstant.AUTH_UNAUTHORIZED_ERR_MSG;
-import static woozlabs.echo.global.constant.GlobalConstant.EMPTY_CHAR;
+import java.util.Arrays;
+import java.util.List;
+
+import static woozlabs.echo.global.constant.GlobalConstant.*;
 
 @Component
 @RequiredArgsConstructor
@@ -19,8 +22,28 @@ public class FirebaseAuthInterceptor implements HandlerInterceptor {
     private final String AUTH_HEADER_PREFIX = "Bearer ";
     private final String FIREBASE_UID_KEY = "uid";
     private final FirebaseTokenVerifier firebaseTokenVerifier;
+    private final List<String> optionalTokenPaths = Arrays.asList("/api/v1/echo/public-share/");
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestPath = request.getRequestURI();
+        String requestMethod = request.getMethod();
+
+        if ("GET".equalsIgnoreCase(requestMethod) && optionalTokenPaths.stream().anyMatch(requestPath::startsWith)) {
+            String authHeader = request.getHeader(AUTH_HEADER_NAME);
+            if (authHeader != null && authHeader.startsWith(AUTH_HEADER_PREFIX)) {
+                String idToken = authHeader.replace(AUTH_HEADER_PREFIX, EMPTY_CHAR);
+                try {
+                    FirebaseToken firebaseToken = firebaseTokenVerifier.verifyToken(idToken);
+                    request.setAttribute(FIREBASE_UID_KEY, firebaseToken.getUid());
+                } catch (FirebaseAuthException e) {
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, AUTH_UNAUTHORIZED_ERR_MSG);
+                    return false;
+                }
+            }
+            return true;
+        }
+
         if(request.getMethod().equals("OPTIONS")){
             return true;
         }
