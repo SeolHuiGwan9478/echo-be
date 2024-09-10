b84aef14da147a83ad642c1c0140784fcc2d98de
DongHyeok Lim <jh07050@gmail.com>
2024-09-10 18:56:59 +0900

    Feat: SharedEmail 기존 POST 로직 분리

--- src/main/java/woozlabs/echo/domain/sharedEmail/controller/SharedInboxController.java
+++ src/main/java/woozlabs/echo/domain/sharedEmail/controller/SharedInboxController.java
@@ -4,11 +4,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import woozlabs.echo.domain.sharedEmail.dto.GetSharedEmailResponseDto;
-import woozlabs.echo.domain.sharedEmail.dto.ShareEmailRequestDto;
+import woozlabs.echo.domain.sharedEmail.dto.*;
 import woozlabs.echo.domain.sharedEmail.dto.thread.ThreadGetResponse;
+import woozlabs.echo.domain.sharedEmail.entity.SharedEmail;
 import woozlabs.echo.domain.sharedEmail.service.SharedInboxService;
 import woozlabs.echo.global.constant.GlobalConstant;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/echo")
@@ -16,6 +18,32 @@ import woozlabs.echo.global.constant.GlobalConstant;
 public class SharedInboxController {
 
     private final SharedInboxService sharedInboxService;
+
+    @PostMapping("/public-share/create")
+    public ResponseEntity<SharedEmail> createSharePost(HttpServletRequest httpServletRequest,
+                                                       @RequestBody CreateSharedRequestDto createSharedRequestDto) {
+        String uid = (String) httpServletRequest.getAttribute(GlobalConstant.FIREBASE_UID_KEY);
+        SharedEmail sharedEmail = sharedInboxService.createSharePost(uid, createSharedRequestDto);
+        return ResponseEntity.ok(sharedEmail);
+    }
+
+    @PostMapping("/public-share/{sharedEmailId}/invite")
+    public ResponseEntity<SharedEmail> inviteToSharedPost(HttpServletRequest httpServletRequest,
+                                                       @PathVariable("sharedEmailId") UUID sharedEmailId,
+                                                       @RequestBody SendSharedEmailInvitationDto sendSharedEmailInvitationDto) {
+        String uid = (String) httpServletRequest.getAttribute(GlobalConstant.FIREBASE_UID_KEY);
+        SharedEmail sharedEmail = sharedInboxService.inviteToSharedPost(uid, sharedEmailId, sendSharedEmailInvitationDto);
+        return ResponseEntity.ok(sharedEmail);
+    }
+
+    @PatchMapping("/public-share/{sharedEmailId}/update")
+    public ResponseEntity<SharedEmail> updateSharedPost(HttpServletRequest httpServletRequest,
+                                                        @PathVariable("sharedEmailId") UUID sharedEmailId,
+                                                        @RequestBody UpdateSharedPostDto updateSharedPostDto) {
+        String uid = (String) httpServletRequest.getAttribute(GlobalConstant.FIREBASE_UID_KEY);
+        SharedEmail updatedSharedEmail = sharedInboxService.updateSharedPost(uid, sharedEmailId, updateSharedPostDto);
+        return ResponseEntity.ok(updatedSharedEmail);
+    }
 
     @PostMapping("/public-share")
     public ResponseEntity<ThreadGetResponse> shareEmail(HttpServletRequest httpServletRequest,
