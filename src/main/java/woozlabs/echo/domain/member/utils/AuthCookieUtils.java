ea29cc8b5e36310a67d8c9bbcefdaa2ca31a0840
DongHyeok Lim <jh07050@gmail.com>
2024-09-15 17:54:09 +0900

    Feat: Auth Logic Logging

--- src/main/java/woozlabs/echo/domain/member/utils/AuthCookieUtils.java
+++ src/main/java/woozlabs/echo/domain/member/utils/AuthCookieUtils.java
@@ -3,10 +3,12 @@ package woozlabs.echo.domain.member.utils;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Arrays;
 import java.util.Optional;
 
+@Slf4j
 public class AuthCookieUtils {
 
     public static void addCookie(HttpServletResponse response, String uid) {
@@ -20,6 +22,7 @@ public class AuthCookieUtils {
     public static Optional<String> getCookieValue(HttpServletRequest request) {
         Cookie[] cookies = request.getCookies();
         if (cookies == null) {
+            log.info("No cookies found.");
             return Optional.empty();
         }
 
