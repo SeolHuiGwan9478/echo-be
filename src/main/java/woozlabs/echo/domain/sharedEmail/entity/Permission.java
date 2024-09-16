5f4d16e99f10acfe551a9f36688bf3c94bef5908
DongHyeok Lim <jh07050@gmail.com>
2024-09-16 11:58:44 +0900

    Feat: PUBLIC_VIEWER 추가 및 Json 역직렬화, 직렬화 시 대문자 처리

--- src/main/java/woozlabs/echo/domain/sharedEmail/entity/Permission.java
+++ src/main/java/woozlabs/echo/domain/sharedEmail/entity/Permission.java
@@ -1,18 +1,10 @@
 package woozlabs.echo.domain.sharedEmail.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public enum Permission {
 
-    @JsonProperty("Editor")
     EDITOR,
-
-    @JsonProperty("Owner")
     OWNER,
-
-    @JsonProperty("Commenter")
     COMMENTER,
-
-    @JsonProperty("Viewer")
-    VIEWER
+    VIEWER,
+    PUBLIC_VIEWER
 }
