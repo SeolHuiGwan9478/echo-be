16d5f25cc9c122e690633b3ce02779cd588b5349
DongHyeok Lim <jh07050@gmail.com>
2024-09-18 18:48:40 +0900

    Feat: Member Info를 위한 쿼리 작성
    Account를 통해 엮여있는 memberAccount들을 추출합니다.
    Member를 통해 해당 Member를 가르키는 Account들을 찾는 쿼리를 작성합니다.
    SDJ 내에서 인지를 하지 못하여 직접 쿼리문을 작성합니다.

--- src/main/java/woozlabs/echo/domain/member/repository/MemberAccountRepository.java
+++ src/main/java/woozlabs/echo/domain/member/repository/MemberAccountRepository.java
@@ -1,13 +1,21 @@
 package woozlabs.echo.domain.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import woozlabs.echo.domain.member.entity.Account;
 import woozlabs.echo.domain.member.entity.Member;
 import woozlabs.echo.domain.member.entity.MemberAccount;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberAccountRepository extends JpaRepository<MemberAccount, Long> {
 
     Optional<MemberAccount> findByMemberAndAccount(Member member, Account account);
+
+    List<MemberAccount> findByAccount(Account account);
+
+    @Query("SELECT ma.account FROM MemberAccount ma WHERE ma.member = :member")
+    List<Account> findAllAccountsByMember(@Param("member") Member member);
 }
